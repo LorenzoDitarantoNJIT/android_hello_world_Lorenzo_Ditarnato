eb16ba2951c3e2211c24fd69d3d7e93e2408ea33
lorenzo <ld6@njit.edu>
2023-01-23 20:13:33 -0500

    Added files

--- app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
+++ app/src/main/java/codepath/demos/helloworlddemo/HelloWorldActivity.java
@@ -1,15 +1,37 @@
 package codepath.demos.helloworlddemo;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HelloWorldActivity extends Activity {
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_hello_world);
+		View button = findViewById(R.id.button);
+		View.OnClickListener btnClick = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+
+				Log.v("hello world", "button clicked");
+				Toast toast = Toast.makeText(getApplicationContext(),"NOOO!!! HE ESCAPED!!", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		};
+
+		button.setOnClickListener(btnClick);
+
+
 	}
 
 	@Override
